f51c50a7adfd48650bfdee6aabe3a392d75b48a1
Nandini2398 <yarranandhini1416@gmail.com>
2021-12-27 20:30:39 +0530

    ADDED:bubblesorting

---
+++ src/com/bridgelabz/algorithmprograms/BubbleSorting.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.algorithmprograms;
+
+public class BubbleSorting {
+		public static void main(String[] args) {
+			BubbleSorting sort = new BubbleSorting();
+			String sentence = "Hi,Welcome,to,Bridgelabz";
+			String array[] = sentence.toLowerCase().split( ",");
+			sort.bubbleSorting(array);
+			for (int i = 0; i < array.length; i++) {
+				System.out.println(array[i]);
+			}
+		}
+		public void bubbleSorting(String [] array) {
+			String temp;
+			for (int i = 0; i < array.length; i++) {
+				for (int j = 0; j < array.length - i - 1; j++) {
+					if (array[j].compareTo(array[j + 1]) > 0) {
+						temp = array[j];
+						array[j] = array[j + 1];
+						array[j + 1] = temp;
+					}
+				}
+			}
+		
+		}
+	}
